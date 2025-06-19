baf92894dd759571f3446307bc7c3d33bd7af7ec
Lozamded <dhiguerafernandez@gmail.com>
2025-06-19 10:57:20 -0400

    MOD: First release

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def package_data(pkg, roots):
 
 setup(
     name='cmmedu-ordertable-xblock',
-    version='1.0.1',
+    version='1.0.0',
     description='XBlock for ordering items',
     packages=[
     ],
